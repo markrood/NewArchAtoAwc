915c632ec56bd336859a60b226600e72343a4d7b
markrood <markdamonrood@gmail.com>
2022-01-25 22:25:35 -0600

    Add files via upload

---
+++ Notification_p_0_1/notification.cpp
@@ -0,0 +1,18 @@
+#include "notification.h"
+
+Notification::Notification(){
+  ;
+}
+
+    void Notification::lowWater(){
+      ;
+    }
+    void Notification::highWater(){
+      ;
+    }
+    void Notification::heartBeat(){
+      ;
+    }
+    void Notification::leak(){
+      ;
+    }
---
+++ Notification_p_0_1/notification.h
@@ -0,0 +1,18 @@
+#ifndef NOT_H_
+#define NOT_H_
+
+
+
+class Notification 
+{
+  public:
+    
+  public:
+    Notification();
+    void lowWater();
+    void highWater();
+    void heartBeat();
+    void leak(); 
+};
+
+#endif

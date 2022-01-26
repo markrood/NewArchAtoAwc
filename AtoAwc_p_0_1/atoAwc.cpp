33d76e9baf43df990a1a8ec840a5ad8b2087c109
markrood <markdamonrood@gmail.com>
2022-01-25 22:25:55 -0600

    Add files via upload

---
+++ AtoAwc_p_0_1/atoAwc.cpp
@@ -0,0 +1,15 @@
+#include "atoAwc.h"
+
+AtoAwc::AtoAwc(){
+  ;
+}
+
+    void AtoAwc::doAto(){
+      ;
+    }
+    void AtoAwc::doAwcEmpty(){
+      ;
+    }
+    void AtoAwc::doAwcFill(){
+      ;
+    }
---
+++ AtoAwc_p_0_1/atoAwc.h
@@ -0,0 +1,17 @@
+#ifndef ATOAWC_H_
+#define ATOAWC_H_
+
+
+
+class AtoAwc 
+{
+  public:
+    
+  public:
+    AtoAwc();
+    void doAto();
+    void doAwcEmpty();
+    void doAwcFill(); 
+};
+
+#endif
